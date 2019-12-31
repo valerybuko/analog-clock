e253f7dbcdfb1271d701dbac4352f5ad67d1a6bf
valery.buka <valpal314@gmail.com>
2019-12-31 12:33:47 +0300

    convert date items to degree

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
-
 let date = new Date();
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
+
+let hrPosition = hr * 360 / 12 + (min * (360 / 60) / 12);
+let minPosition = min * 360 / 60 + (sec * (360 / 60) / 60);
+let secPosition = sec * 360 / 60;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
